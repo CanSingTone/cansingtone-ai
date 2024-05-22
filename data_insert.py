6af4651004d961710e982a3111890791bf9c15a8
ysshwan147 <chick123159147@gmail.com>
2024-05-23 00:02:37 +0900

    Create data_insert.py
    insert csv data to DB

---
+++ data_insert.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import mysql.connector
+from mysql.connector import errorcode
+
+# CSV 파일 읽기
+file_path = 'song_test.csv'
+df = pd.read_csv(file_path, encoding='utf-8-sig')
+
+# song_id 열 추가: 1부터 시작하는 순차적인 정수 값
+df['song_id'] = range(4, len(df) + 4)
+
+# gender_code -> artist_gender, genre_code -> genre 변환
+df['artist_gender'] = df['gender_code']
+df['genre'] = df['genre_code']
+
+# karaoke_vid_id -> mr_vid_url, karaoke_vid_id -> song_vid_url
+df['mr_vid_url'] = df['karaoke_vid_id']
+df['song_vid_url'] = df['karaoke_vid_id']
+
+# 필요한 열만 선택하여 새로운 DataFrame 생성
+db_df = df[['artist_gender', 'genre', 'highest_note', 'lowest_note', 'song_id', 'artist', 'mr_vid_url', 'song_title', 'song_vid_url', 'album_image_url']].copy()
+
+# 열 이름을 데이터베이스 필드 이름에 맞게 변경
+db_df.rename(columns={
+    'artist_gender': 'artist_gender',
+    'genre': 'genre',
+    'highest_note': 'highest_note',
+    'lowest_note': 'lowest_note',
+    'song_id': 'song_id',
+    'artist': 'artist',
+    'mr_vid_url': 'mr_vid_url',
+    'song_title': 'song_title',
+    'song_vid_url': 'song_vid_url',
+    'album_image_url': 'album_image'
+}, inplace=True)
+
+# MySQL 데이터베이스 연결 설정
+config = {
+    'user': 'user',
+    'password': 'jsql1445',
+    'host': '13.125.27.204',
+    'port': 3306,
+    'database': 'cansingtone-schema',
+    'raise_on_warnings': True
+}
+
+conn = None
+cursor = None
+
+try:
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+
+    # DataFrame 데이터를 MySQL 테이블에 삽입
+    for index, row in db_df.iterrows():
+        insert_query = """
+        INSERT INTO song (artist_gender, genre, highest_note, lowest_note, song_id, artist, mr_vid_url, song_title, song_vid_url, album_image)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(insert_query, (
+            row['artist_gender'], row['genre'], row['highest_note'], row['lowest_note'],
+            row['song_id'], row['artist'], row['mr_vid_url'], row['song_title'],
+            row['song_vid_url'], row['album_image']
+        ))
+
+    # 커밋
+    conn.commit()
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("사용자 이름 또는 비밀번호가 잘못되었습니다.")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("데이터베이스가 존재하지 않습니다.")
+    else:
+        print(err)
+finally:
+    if cursor is not None:
+        cursor.close()
+    if conn is not None:
+        conn.close()
