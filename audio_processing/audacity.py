5ca9634451bf46e2374e5491157553bfa4d74e23
ysshwan147 <chick123159147@gmail.com>
2024-06-05 23:14:32 +0900

    Update audacity.py
    update vad

--- audio_processing/audacity.py
+++ audio_processing/audacity.py
@@ -12,16 +12,22 @@ def execute_audacity_command(command):
         raise Exception(f"Command failed: {command}\nResponse: {response}")
     return response
 
-input_path = 'output'
-output_path = 'output_vad'
+input_path = 'sample_vocal'
+output_path = 'sample_vad1'
 
 songs = [path for path in os.listdir(input_path) if not path.startswith('.')]
+processed_songs = [path for path in os.listdir(output_path) if not path.startswith('.')]
 
 for song in songs:
-    input_name = song
-    input_song_path = os.path.join(input_path, input_name)
+    input_song_path = os.path.join(input_path, song)
     output_name = song[:-4] + " pyaudacity.mp3"
     output_song_path = os.path.join(output_path, output_name)
+
+    print(f"{output_name}: ", end="")
+
+    if output_name in processed_songs:
+        print("already exists")
+        continue
 
     input_path_absolute = os.path.abspath(input_song_path)
     output_path_absolute = os.path.abspath(output_song_path)
@@ -38,6 +44,9 @@ for song in songs:
         execute_audacity_command('Amplify: Ratio="0.075"')
         time.sleep(1)
         execute_audacity_command(f'Export2: Filename="{output_path_absolute}"')
+        time.sleep(3)
+        execute_audacity_command('RemoveTracks')
+        time.sleep(1)
 
     except Exception as e:
         print(f"An error occurred: {e}")
